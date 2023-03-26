fb7718a089dd90bc4d5041d2d1bc5a0a7d02d5d2
allencarne <allencarne@yahoo.com>
2023-03-26 12:19:35 -0700

    Snail attack

--- Assets/_Scripts/Snail.cs
+++ Assets/_Scripts/Snail.cs
@@ -4,5 +4,36 @@ using UnityEngine;
 
 public class Snail : Enemy
 {
+    [SerializeField] GameObject shmackTelegraph;
+    [SerializeField] float shmackCoolDown;
 
+    protected override void EnemyAttackState()
+    {
+        base.EnemyAttackState();
+
+        if (canAttack)
+        {
+            // Prevents from running more than once
+            canAttack = false;
+
+            // Animation
+            enemyAnimator.Play("Shmack");
+
+            // Prevents enemy from being moved while casting
+            enemyRB.isKinematic = true;
+
+            // Attack Prefab
+            Instantiate(shmackTelegraph, transform.position, enemyAimer.rotation);
+
+            // CoolDown
+            StartCoroutine(ShmackCoolDown());
+        }
+    }
+
+    IEnumerator ShmackCoolDown()
+    {
+        yield return new WaitForSeconds(shmackCoolDown);
+
+        canAttack = true;
+    }
 }
