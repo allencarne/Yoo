09c48c8b4136f28a0294e2bcaffb6012a354146f
allencarne <allencarne@yahoo.com>
2023-03-26 23:40:49 -0700

    Player Script

--- Assets/_Scripts/Player/Player.cs
+++ Assets/_Scripts/Player/Player.cs
@@ -4,5 +4,133 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    enum PlayerState
+    {
+        Spawn,
+        Idle,
+        Run,
+        Hurt,
+        Death,
+        BasicAttack,
+        BasicAttack2,
+        BasicAttack3,
+        Ability,
+        Mobility,
+        Defensive,
+        Utility,
+        Ultimate,
+    }
 
+    PlayerState state = PlayerState.Spawn;
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case PlayerState.Spawn:
+                PlayerSpawnState();
+                break;
+            case PlayerState.Idle:
+                PlayerIdleState();
+                break;
+            case PlayerState.Run:
+                PlayerRunState();
+                break;
+            case PlayerState.Hurt:
+                PlayerHurtState();
+                break;
+            case PlayerState.Death:
+                PlayerDeathState();
+                break;
+            case PlayerState.BasicAttack:
+                PlayerBasicAttackState();
+                break;
+            case PlayerState.BasicAttack2:
+                PlayerBasicAttack2State();
+                break;
+            case PlayerState.BasicAttack3:
+                PlayerBasicAttack3State();
+                break;
+            case PlayerState.Ability:
+                PlayerAbilityState();
+                break;
+            case PlayerState.Mobility:
+                PlayerMobilityState();
+                break;
+            case PlayerState.Defensive:
+                PlayerDefensiveState();
+                break;
+            case PlayerState.Utility:
+                PlayerUtilityState();
+                break;
+            case PlayerState.Ultimate:
+                PlayerUltimateState();
+                break;
+        }
+    }
+
+    public void PlayerSpawnState()
+    {
+
+    }
+
+    public void PlayerIdleState()
+    {
+
+    }
+
+    public void PlayerRunState()
+    {
+
+    }
+
+    public void PlayerHurtState()
+    {
+
+    }
+
+    public void PlayerDeathState()
+    {
+
+    }
+
+    public void PlayerBasicAttackState()
+    {
+
+    }
+
+    public void PlayerBasicAttack2State()
+    {
+
+    }
+
+    public void PlayerBasicAttack3State()
+    {
+
+    }
+
+    public void PlayerAbilityState()
+    {
+
+    }
+
+    public void PlayerMobilityState()
+    {
+
+    }
+
+    public void PlayerDefensiveState()
+    {
+
+    }
+
+    public void PlayerUtilityState()
+    {
+
+    }
+
+    public void PlayerUltimateState()
+    {
+
+    }
 }
