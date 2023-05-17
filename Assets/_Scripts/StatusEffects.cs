e1af8334c3c831ff00b643f60b0f1b85f9fdebe8
allencarne <allencarne@yahoo.com>
2023-05-16 22:24:29 -0700

    Status Effects Animations

--- Assets/_Scripts/StatusEffects.cs
+++ Assets/_Scripts/StatusEffects.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class StatusEffects : MonoBehaviour
 {
+    [Header("Buffs")]
+    public GameObject agilityIcon;
+    public GameObject alacrityIcon;
+    public GameObject hasteIcon;
+    public GameObject mightIcon;
+    public GameObject protectionIcon;
+    public GameObject regenerationIcon;
 
+    [Header("Debuffs")]
+    public GameObject bleedIcon;
+    public GameObject exhaustIcon;
+    public GameObject impedeIcon;
+    public GameObject slowIcon;
+    public GameObject vulnerabilityIcon;
+    public GameObject weaknessIcon;
+
+    [Header("CC")]
+    public GameObject incapacitateIcon;
+    public GameObject knockBackIcon;
+    public GameObject knockUpIcon;
+    public GameObject silenceIcon;
+    public GameObject snareIcon;
+    public GameObject stunIcon;
 }
